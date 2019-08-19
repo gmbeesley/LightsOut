555273ab8af23c9d84850b14b156befb6d3a565c
Graham Beesley <graham.beesley@gmail.com>
2019-08-19 20:43:32 +0100

    Updated unit tests

--- LightsOutTests/LightGridTests.cs
+++ LightsOutTests/LightGridTests.cs
@@ -23,11 +23,13 @@ namespace LightsOut.Tests
             LightGrid lightGrid = new LightGrid();
             lightGrid.InitialiseLightGrid();
 
+            Assert.AreNotEqual(0, lightGrid.LightsOnCount);
+            Assert.AreNotEqual(0, lightGrid.LightsOnGrid.Cast<bool>().ToArray().Count(x => x.Equals(true)));
             Assert.AreEqual(lightGrid.LightsOnGrid.Cast<bool>().ToArray().Count(x => x.Equals(true)), lightGrid.LightsOnCount);
         }
 
         [TestMethod()]
-        public void LightGridProcessLightSwitchTest()
+        public void LightGridProcessLightSwitchTest_00()
         {
             LightGrid lightGrid = new LightGrid();
 
@@ -86,6 +88,12 @@ namespace LightsOut.Tests
             Assert.AreEqual(false, lightGrid.LightsOnGrid[3, 4]);
             Assert.AreEqual(false, lightGrid.LightsOnGrid[4, 4]);
             Assert.AreEqual(0, lightGrid.LightsOnCount);
+        }
+
+        [TestMethod()]
+        public void LightGridProcessLightSwitchTest__22()
+        {
+            LightGrid lightGrid = new LightGrid();
 
             lightGrid.ProcessLightSwitch(2, 2);
             Assert.AreEqual(false, lightGrid.LightsOnGrid[0, 0]);
@@ -142,6 +150,12 @@ namespace LightsOut.Tests
             Assert.AreEqual(false, lightGrid.LightsOnGrid[3, 4]);
             Assert.AreEqual(false, lightGrid.LightsOnGrid[4, 4]);
             Assert.AreEqual(0, lightGrid.LightsOnCount);
+        }
+
+        [TestMethod()]
+        public void LightGridProcessLightSwitchTest__44()
+        {
+            LightGrid lightGrid = new LightGrid();
 
             lightGrid.ProcessLightSwitch(4, 4);
             Assert.AreEqual(false, lightGrid.LightsOnGrid[0, 0]);
@@ -198,6 +212,69 @@ namespace LightsOut.Tests
             Assert.AreEqual(false, lightGrid.LightsOnGrid[3, 4]);
             Assert.AreEqual(false, lightGrid.LightsOnGrid[4, 4]);
             Assert.AreEqual(0, lightGrid.LightsOnCount);
+        }
+
+        [TestMethod()]
+        public void LightGridProcessLightSwitchTest__22_Alt()
+        {
+            LightGrid lightGrid = new LightGrid();
+            lightGrid.LightsOnGrid[2, 2] = true;
+
+            lightGrid.ProcessLightSwitch(2, 2);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[0, 0]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[1, 0]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[2, 0]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[3, 0]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[4, 0]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[0, 1]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[1, 1]);
+            Assert.AreEqual(true, lightGrid.LightsOnGrid[2, 1]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[3, 1]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[4, 1]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[0, 2]);
+            Assert.AreEqual(true, lightGrid.LightsOnGrid[1, 2]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[2, 2]);
+            Assert.AreEqual(true, lightGrid.LightsOnGrid[3, 2]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[4, 2]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[0, 3]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[1, 3]);
+            Assert.AreEqual(true, lightGrid.LightsOnGrid[2, 3]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[3, 3]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[4, 3]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[0, 4]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[1, 4]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[2, 4]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[3, 4]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[4, 4]);
+            Assert.AreEqual(4, lightGrid.LightsOnCount);
+
+            lightGrid.ProcessLightSwitch(2, 2);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[0, 0]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[1, 0]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[2, 0]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[3, 0]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[4, 0]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[0, 1]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[1, 1]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[2, 1]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[3, 1]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[4, 1]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[0, 2]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[1, 2]);
+            Assert.AreEqual(true, lightGrid.LightsOnGrid[2, 2]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[3, 2]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[4, 2]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[0, 3]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[1, 3]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[2, 3]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[3, 3]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[4, 3]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[0, 4]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[1, 4]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[2, 4]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[3, 4]);
+            Assert.AreEqual(false, lightGrid.LightsOnGrid[4, 4]);
+            Assert.AreEqual(1, lightGrid.LightsOnCount);
         }
     }
 }
